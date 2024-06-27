2cf863d4a62e7ecdc59f568f21670ae63358b8d0
wjs <13193669+a-cook-who-learns-programming@user.noreply.gitee.com>
2024-06-27 15:01:19 +0800

    前后端空壳

--- src/main.js
+++ src/main.js
@@ -1,19 +1,19 @@
 import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
+
 import App from './App.vue'
 import router from './router'
-import '@/assets/style.css'
-import * as ElementPlusIconsVue from '@element-plus/icons-vue'
-import { ref } from 'vue';
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
+import axios from "axios";
+
 const app = createApp(App)
-for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-    app.component(key, component)
-  }
+
+app.use(ElementPlus)
 app.use(createPinia())
 app.use(router)
-app.use(ElementPlus)
+
+
 app.mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,51 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig ,loadEnv} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+
+export default defineConfig(({ command, mode }) => {
+  // 根据当前工作目录中的 `mode` 加载 .env 文件
+  // 设置第三个参数为 '' 来加载所有环境变量，而不管是否有 `VITE_` 前缀。
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    // vite 配置
+    plugins: [
+      vue(),
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    server: {
+      // 服务器端口号配置，默认为 8888，如果 VITE_APP_PORT 配置了其他值，则使用其他值
+      port: Number(env.VITE_APP_PORT) || 3000,
+      
+      // 代理配置，解决前端跨域问题
+      proxy: {
+        // 使用环境变量中的 VITE_APP_API 作为代理的路径前缀，如 "/test"
+        [env.VITE_APP_API]: {
+          // 代理目标地址，即后端服务器的地址
+          target: env.VITE_APP_API_BASEURL,
+          
+          // 是否更改请求的源头地址，为 true 时会将请求头中的 Origin 修改为目标地址
+          changeOrigin: true,
+
+          // 重写路径，去掉路径中的前缀部分
+          rewrite: path => path.replace(new RegExp(`^${env.VITE_APP_API}`), ''),
+        },
+      },
     }
   }
 })
+
